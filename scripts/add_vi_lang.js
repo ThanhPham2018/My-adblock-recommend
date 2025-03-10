c5a6c997a6cc045f1237dda8aaf6282ae247b222
ThanhPham2018 <45378374+ThanhPham2018@users.noreply.github.com>
2025-03-10 23:55:09 +0700

    Update add_vi_lang.js

--- scripts/add_vi_lang.js
+++ scripts/add_vi_lang.js
@@ -1,28 +1,19 @@
 // ==UserScript==
 // @name         Thêm tham số ngôn ngữ tiếng Việt vào NotebookLM và AI Studio
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Tự động thêm ?hl=vi vào URL khi truy cập NotebookLM và AI Studio
 // @author       You
+// @downloadURL     https://raw.githubusercontent.com/ThanhPham2018/My-adblock-recommend/refs/heads/main/scripts/add_vi_lang.js
+// @updateURL       https://raw.githubusercontent.com/ThanhPham2018/My-adblock-recommend/refs/heads/main/scripts/add_vi_lang.js
+// @homepageURL     https://github.com/ThanhPham2018/My-adblock-recommend/tree/main/scripts
+
 // @match        *://*.notebooklm.google.com/*
 // @match        *://*.aistudio.google.com/*
 // @match        *://*.gemini.google.com/*
 // @match        https://notebooklm.google.com/*
 // @match        https://aistudio.google.com/*
 // @match        https://gemini.google.com/*
-// @match        *://notebooklm.google.com*
-// @match        *://aistudio.google.com*
-// @match        *://gemini.google.com*
-// @match        *://iask.ai*
-// @match        *://you.com*
-// @match        *://www.genspark.ai*
-// @match        *://www.perplexity.ai*
-// @match        *://x.com*
-// @match        *://chatgpt.com*
-// @match        *://www.meta.ai*
-// @match        *://chat.qwen.ai*
-// @match        *://chat.deepseek.com*
-
 
 // @grant        none
 // ==/UserScript==
