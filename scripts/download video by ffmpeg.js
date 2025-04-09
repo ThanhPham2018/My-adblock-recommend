b177087804beb3d5ce39135d37ef852a8a9fbdb6
ThanhPham2018 <45378374+ThanhPham2018@users.noreply.github.com>
2025-04-09 23:15:29 +0700

    Update and rename download video by ffmpeg to download video by ffmpeg.js

--- scripts/download video by ffmpeg.js
+++ scripts/download video by ffmpeg.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Video URL Extractor
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Tự động lấy URL video và tạo lệnh ffmpeg
 // @author       You
-// @match        *://*/*
+// @match        https://*.microsoftstream.com/*
+// @match        https://*.sharepoint.com/*
+// @match        https://*.office.com/*
 // @grant        GM_setClipboard
 // ==/UserScript==
 
